75e0069692aa636ac874dfa4712539ae15ac1219
Evana-ad <evana.carrier@gmail>
2026-03-17 10:02:32 +0530

    UC2: Initialising Basic Room Types & Static Availability

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -1,11 +1,35 @@
+import java.util.*;
+
+class Room {
+
+    int roomNumber;
+    String type;
+    boolean available;
+
+    Room(int roomNumber, String type, boolean available) {
+        this.roomNumber = roomNumber;
+        this.type = type;
+        this.available = available;
+    }
+
+    void displayRoom() {
+        System.out.println("Room No: " + roomNumber +
+                " | Type: " + type +
+                " | Available: " + available);
+    }
+}
+
 public class HotelBookingApp {
 
     public static void main(String[] args) {
 
-        System.out.println("==================================");
-        System.out.println("      Welcome to Book My Stay      ");
-        System.out.println("   Hotel Booking System v1.0       ");
-        System.out.println("==================================");
+        Room r1 = new Room(101, "AC", true);
+        Room r2 = new Room(102, "NON-AC", false);
+        Room r3 = new Room(103, "DELUXE", true);
+
+        r1.displayRoom();
+        r2.displayRoom();
+        r3.displayRoom();
 
     }
-            }
+}
