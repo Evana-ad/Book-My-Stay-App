7b8e82838d61705a228973d37b1b37cbaa9455e7
Evana-ad <evana.carrier@gmail>
2026-03-17 10:28:40 +0530

    UC4: Room Search & Availability Check

--- src/HotelBookingApp.java
+++ src/HotelBookingApp.java
@@ -2,38 +2,82 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Use Case 3 – Centralized Room Inventory Management
- * Demonstrates HashMap based inventory
- * @version 3.1
+ * Use Case 4 – Room Search & Availability Check
+ * Read-only search using centralized inventory
+ * @version 4.1
  */
+
+abstract class Room {
+
+    String type;
+    double price;
+
+    Room(String type, double price) {
+        this.type = type;
+        this.price = price;
+    }
+
+    void displayDetails() {
+        System.out.println("Room Type : " + type);
+        System.out.println("Price     : " + price);
+    }
+}
+
+class SingleRoom extends Room {
+    SingleRoom() {
+        super("Single Room", 2000);
+    }
+}
+
+class DoubleRoom extends Room {
+    DoubleRoom() {
+        super("Double Room", 3500);
+    }
+}
+
+class SuiteRoom extends Room {
+    SuiteRoom() {
+        super("Suite Room", 6000);
+    }
+}
 
 class RoomInventory {
 
     private Map<String, Integer> availability;
 
     RoomInventory() {
-
         availability = new HashMap<>();
-
         availability.put("Single Room", 5);
         availability.put("Double Room", 3);
-        availability.put("Suite Room", 2);
+        availability.put("Suite Room", 0);   // example unavailable
     }
 
     int getAvailability(String type) {
         return availability.get(type);
     }
+}
 
-    void updateAvailability(String type, int count) {
-        availability.put(type, count);
+class RoomSearchService {
+
+    private RoomInventory inventory;
+
+    RoomSearchService(RoomInventory inventory) {
+        this.inventory = inventory;
     }
 
-    void displayInventory() {
+    void searchAvailableRooms(Room[] rooms) {
 
-        System.out.println("===== Centralized Room Inventory v3.1 =====");
+        System.out.println("===== Available Rooms v4.1 =====");
 
-        for (String type : availability.keySet()) {
-            System.out.println(type + " Available : " + availability.get(type));
+        for (Room room : rooms) {
+
+            int count = inventory.getAvailability(room.type);
+
+            if (count > 0) {
+                room.displayDetails();
+                System.out.println("Available : " + count);
+                System.out.println();
+            }
         }
     }
 }
@@ -44,6 +88,14 @@ public class HotelBookingApp {
 
         RoomInventory inventory = new RoomInventory();
 
-        inventory.displayInventory();
+        Room[] rooms = {
+                new SingleRoom(),
+                new DoubleRoom(),
+                new SuiteRoom()
+        };
+
+        RoomSearchService search = new RoomSearchService(inventory);
+
+        search.searchAvailableRooms(rooms);
     }
 }
